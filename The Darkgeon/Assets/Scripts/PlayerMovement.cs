9885c847a7c9d9d2d7e80eb2c680108fe57c791a
Constace <vuongnhat777@gmail.com>
2022-08-18 20:22:37 +0700

    Added Player and Environment

---
+++ The Darkgeon/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+	// References.
+	[SerializeField] private CharacterController2D controller;
+
+	// Fields.
+	public float moveSpeed = 30f;
+
+	float horizontalMove;
+	bool jump = false;
+	bool crouch = false;
+
+	private void Awake()
+	{
+		controller = GetComponent<CharacterController2D>();
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+		horizontalMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+
+		if (Input.GetButtonDown("Jump"))
+			jump = true;
+
+		if (Input.GetButtonDown("Crouch"))
+			crouch = true;
+		else if (Input.GetButtonUp("Crouch"))
+			crouch = false;
+	}
+
+	private void FixedUpdate()
+	{
+		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+		jump = false;
+	}
+}
